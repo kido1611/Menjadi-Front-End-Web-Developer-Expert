ede5854b974dbce383c3c16c3c85246a37ad8772
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2021-04-18 21:27:55 +0700

    Submission 1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,25 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
-
+import '../scripts/restaurant-item.js';
 import data from '../DATA.json';
 
-console.log('Hello Coders! :)');
+const hamburgerButtonElement = document.querySelector("#hamburger");
+const drawerElement = document.querySelector("#sidenav");
 
-console.log(data);
+hamburgerButtonElement.addEventListener("click", event => {
+    drawerElement.classList.toggle("open");
+    event.stopPropagation();
+});
+
+const restaurantList = document.querySelector(".restaurant__list");
+data.restaurants.forEach(restaurant => {
+    const element = document.createElement("restaurant-item");
+    element.setAttribute('id', restaurant.id);
+    element.setAttribute('name', restaurant.name);
+    element.setAttribute('picture', restaurant.pictureId);
+    element.setAttribute('location', restaurant.city);
+    element.setAttribute('rating', restaurant.rating);
+    element.setAttribute('description', restaurant.description);
+
+    restaurantList.appendChild(element);
+});
