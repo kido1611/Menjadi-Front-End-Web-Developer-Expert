527e46634749924c4dfeeb5bcbd93242543a95cf
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2021-04-26 05:18:43 +0700

    Change BASE_IMAGE_URL with BASE_IMAGE_SMALL_URL an BASE_IMAGE_MEDIUM_URL

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,8 @@
 const CONFIG = {
   KEY: '12345',
   BASE_URL: 'https://restaurant-api.dicoding.dev',
-  BASE_IMAGE_URL: (pictureId) => `https://restaurant-api.dicoding.dev/images/medium/${pictureId}`,
+  BASE_IMAGE_SMALL_URL: (pictureId) => `https://restaurant-api.dicoding.dev/images/small/${pictureId}`,
+  BASE_IMAGE_MEDIUM_URL: (pictureId) => `https://restaurant-api.dicoding.dev/images/medium/${pictureId}`,
   CACHE_NAME: new Date().toISOString(),
   DATABASE_NAME: 'resto-database',
   DATABASE_VERSION: 1,
