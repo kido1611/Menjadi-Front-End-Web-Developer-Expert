6a253a77aeda30348a067e742f6315304660875c
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2021-04-28 05:45:44 +0700

    Update webpack common config file

--- webpack.netlify.js
+++ webpack.netlify.js
@@ -1,13 +1,8 @@
 const { merge } = require('webpack-merge');
-const path = require('path');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   module: {
     rules: [
       {
